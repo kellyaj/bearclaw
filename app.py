497006438c3bf701f727d8b406a71d7649d9b5ca
Andrew Kelly <akelly@8thlight.com>
2014-01-20 12:27:43 -0600

    introduces templates for index.html

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask import render_template
 from fedex import FedexTracker
 app = Flask(__name__)
 
@@ -6,7 +7,7 @@ app = Flask(__name__)
 def root():
     fedex_tracker = FedexTracker("9611804258025804723257")
     return_data = fedex_tracker.last_location() + " on " + fedex_tracker.last_checkin()
-    return return_data
+    return render_template('index.html', data=return_data)
 
 if __name__ == "__main__":
   app.run(debug=True)
