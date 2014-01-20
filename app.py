20dadc295da816805ba5a97fccd87d9431ee6ab7
Andrew Kelly <akelly@8thlight.com>
2014-01-20 15:56:36 -0600

    allows use of multiple tracking numbers

--- app.py
+++ app.py
@@ -5,9 +5,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def root():
-    fedex_tracker = FedexTracker("9611804258025804723257")
-    return_data = fedex_tracker.last_location() + " on " + fedex_tracker.last_checkin()
-    return render_template('index.html', data=return_data)
+    fedex_tracker = FedexTracker(["9611804258025804723257"])
+    return render_template('index.html', entries=fedex_tracker.entries)
 
 if __name__ == "__main__":
   app.run(debug=True)
--- fedex.py
+++ fedex.py
@@ -3,44 +3,57 @@ import json
 
 class FedexTracker(object):
 
-    def __init__(self, tracking_number):
-      self.tracking_number = tracking_number
+    def __init__(self, tracking_numbers):
+      self.tracking_numbers = tracking_numbers
+      self.request_datas = []
+      self.raw_data_responses = []
+      self.entries = []
       self.create_request_data()
       self.make_request()
+      self.generate_entry_dictionary()
 
     def create_request_data(self):
-        self.request_data = {
-            'data': json.dumps({
-                'TrackPackagesRequest': {
-                    'appType': 'wtrk',
-                    'uniqueKey': '',
-                    'processingParameters': {
-                        'anonymousTransaction': True,
-                        'clientId': 'WTRK',
-                        'returnDetailedErrors': True,
-                        'returnLocalizedDateTime': False
-                        },
-                    'trackingInfoList': [{
-                        'trackNumberInfo': {
-                            'trackingNumber': self.tracking_number,
-                            'trackingQualifier': '',
-                            'trackingCarrier': ''
-                            }
-                        }]
-                    }
-                }),
-            'action': 'trackpackages',
-            'locale': 'en_US',
-            'format': 'json',
-            'version': 99
-        }
+      for number in self.tracking_numbers:
+          data = {
+              'data': json.dumps({
+                  'TrackPackagesRequest': {
+                      'appType': 'wtrk',
+                      'uniqueKey': '',
+                      'processingParameters': {
+                          'anonymousTransaction': True,
+                          'clientId': 'WTRK',
+                          'returnDetailedErrors': True,
+                          'returnLocalizedDateTime': False
+                          },
+                      'trackingInfoList': [{
+                          'trackNumberInfo': {
+                              'trackingNumber': number,
+                              'trackingQualifier': '',
+                              'trackingCarrier': ''
+                              }
+                          }]
+                      }
+                  }),
+              'action': 'trackpackages',
+              'locale': 'en_US',
+              'format': 'json',
+              'version': 99
+          }
+          self.request_datas.append(data)
 
     def make_request(self):
-        self.rawData = requests.post('https://www.fedex.com/trackingCal/track', self.request_data).json()
+        for request_data in self.request_datas:
+            self.raw_data_responses.append(requests.post('https://www.fedex.com/trackingCal/track', request_data).json())
 
-    def last_location(self):
-        return self.rawData["TrackPackagesResponse"]["packageList"][0]["scanEventList"][0]["scanLocation"]
+    def last_location(self, raw_data):
+        return raw_data["TrackPackagesResponse"]["packageList"][0]["scanEventList"][0]["scanLocation"]
 
-    def last_checkin(self):
-        return self.rawData["TrackPackagesResponse"]["packageList"][0]["scanEventList"][0]["date"]
+    def last_checkin(self, raw_data):
+        return raw_data["TrackPackagesResponse"]["packageList"][0]["scanEventList"][0]["date"]
 
+    def generate_entry_dictionary(self):
+        for raw_data in self.raw_data_responses:
+            entry = {}
+            entry['last_location'] = self.last_location(raw_data)
+            entry['last_checkin'] = self.last_checkin(raw_data)
+            self.entries.append(entry)
