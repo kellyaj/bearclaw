a7a1f23dd43e9c13bb28a8d0d3cd1d4eee1da552
Andrew Kelly <akelly@8thlight.com>
2014-01-24 14:19:54 -0600

    route for listening to post

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask import request
 from flask import render_template
 from fedex import FedexTracker
 app = Flask(__name__)
@@ -8,6 +9,13 @@ def root():
     nums = ["9611804258025804723257", "9611804258025804723257", "9611804183634603474634", "123456789012"]
     fedex_tracker = FedexTracker(nums)
     return render_template('index.html', entries=fedex_tracker.entries)
+
+@app.route("/create", methods=['POST'])
+def create():
+    with open("numbers.csv", "ab") as fo:
+        fo.write(request.data + "\n")
+    return "ok"
+
 
 if __name__ == "__main__":
   app.run(debug=True)
