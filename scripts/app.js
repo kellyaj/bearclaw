e880b2bd556c6a9ed3f8bc517a6778d27e19fdc9
Andrew Kelly <akelly@8thlight.com>
2014-01-25 18:05:43 -0600

    removes old js files

--- scripts/app.js
+++
@@ -1,8 +0,0 @@
-Trello.authorize({
-  name: "Bearclaw",
-  scope: {read: true},
-  expiration: "never"
-});
-
-var cardTrapper = new CardTrapper(Trello);
-cardTrapper.getCards();
--- server.js
+++
@@ -1,8 +0,0 @@
-var connect = require('connect');
-var port = 8090;
-
-process.stdout.write("Running server on port " + port + "...\r\n");
-
- connect.createServer(
-     connect.static(__dirname)
-).listen(port);
