ee745832415a508dcffd57ebb64a103b41d694f4
Andrew Kelly <akelly@8thlight.com>
2014-01-30 12:10:01 -0600

    uses real data from the page

--- static/app.js
+++ static/app.js
@@ -2,7 +2,7 @@ $('button').on('click', function(event) {
   event.preventDefault();
   trackingNumber = $(event.target).data('id');
   removeUrl = "http://localhost:5000/delete"
-  data = {"thing": "test"}
+  data = {"number": trackingNumber}
 
   $.ajax({
     url: removeUrl,
